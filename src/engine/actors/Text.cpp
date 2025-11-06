d11fd40c050eb060f550663bbe2332004fa3f518
MegaMech <7255464+MegaMech@users.noreply.github.com>
2025-11-05 22:35:18 -0700

    Work now

--- src/engine/actors/Text.cpp
+++ src/engine/actors/Text.cpp
@@ -1,4 +1,5 @@
 #include <libultraship.h>
+#include <libultra/gbi.h>
 
 #include "Text.h"
 
@@ -43,6 +44,10 @@ AText::AText(const SpawnParams& params) : AActor(params) {
     Text = "My Text";
 }
 
+void AText::Init() {
+    AText::Print3D((char*)Text, 0, 1);
+}
+
 bool AText::IsMod() { return true; }
 
 void AText::SetSpawnParams(SpawnParams& params) {
@@ -66,21 +71,27 @@ void AText::Tick() {
 }
 
 void AText::FollowPlayer() {
-    Pos[0] = gPlayers[PlayerIndex].pos[0];
+    Pos[0] = gPlayers[PlayerIndex].pos[0] - ((s16) (WidthOffset / 4)); // Center above player
     Pos[1] = gPlayers[PlayerIndex].pos[1] + HeightOffset;
     Pos[2] = gPlayers[PlayerIndex].pos[2];
 }
 
 void AText::Draw(Camera* camera) {
-    f32 distance = is_within_render_distance(camera->pos, Pos, camera->rot[1], 2500.0f,
-                                             gCameraZoom[camera - camera1], 9000000.0f);
+    f32 distance = is_within_render_distance(camera->pos, (float*)&Pos[0], camera->rot[1], 800.0f,
+                                             gCameraZoom[camera - camera1], 14000.0f);
 
-    // if (distance < 122500.0f) {
-        AText::Print3D(camera, (char*)Text, 0, 0.55f, 0.55f, 1);
-    //}
+    if (PlayerIndex == camera->playerId) {
+        return; // Do not draw the local players own name
+    }
+
+    if (distance < -1.0f) {
+        //printf("list %d\n", TextureList.size());
+        //AText::Print3D(camera, (char*)Text, 0, 0.55f, 0.55f, 1);
+        AText::SetupMatrix(camera);
+    }
 }
 
-void AText::Print3D(Camera* camera, char* text, s32 tracking, f32 scaleX, f32 scaleY, s32 mode) {
+void AText::Print3D(char* text, s32 tracking, s32 mode) {
     char* temp_string = text;
     s32 stringWidth = 0;
     s32 glyphIndex;
@@ -97,12 +108,12 @@ void AText::Print3D(Camera* camera, char* text, s32 tracking, f32 scaleX, f32 sc
     // @port Skip Interpolation, if interpolated later remove this tag
     FrameInterpolation_ShouldInterpolateFrame(false);
 
-    while (*temp_string != 0) {
+    while (*temp_string != '\0') {
         glyphIndex = char_to_glyph_index(temp_string);
         if (glyphIndex >= 0) {
-            stringWidth += ((gGlyphDisplayWidth[glyphIndex] + tracking) * scaleX);
+            stringWidth += ((gGlyphDisplayWidth[glyphIndex] + tracking) * ScaleX);
         } else if ((glyphIndex != -2) && (glyphIndex == -1)) {
-            stringWidth += ((tracking + 7) * scaleX);
+            stringWidth += ((tracking + 7) * ScaleX);
         } else {
             return;
         }
@@ -135,18 +146,19 @@ void AText::Print3D(Camera* camera, char* text, s32 tracking, f32 scaleX, f32 sc
         sp60 = 2;
     }
 
-    gSPDisplayList(gDisplayListHead++, (Gfx*)D_020077A8);
-    while (*text != 0) {
+    //gSPDisplayList(gDisplayListHead++, (Gfx*)D_020077A8);
+    while (*text != '\0') {
         glyphIndex = char_to_glyph_index(text);
         if (glyphIndex >= 0) {
             // load_menu_img(gGlyphTextureLUT[glyphIndex]);
-            gDisplayListHead = AText::PrintLetter3D(camera, gDisplayListHead, gGlyphTextureLUT[glyphIndex],
-                                            column, row, sp60, scaleX, scaleY);
-            column = column + (s32) ((gGlyphDisplayWidth[glyphIndex] + tracking) * scaleX);
+            AText::PrintLetter3D(gGlyphTextureLUT[glyphIndex],
+                                            column, row, sp60);
+            column = column + (s32) ((gGlyphDisplayWidth[glyphIndex] + tracking) * ScaleX);
         } else if ((glyphIndex != -2) && (glyphIndex == -1)) {
-            column = column + (s32) ((tracking + 7) * scaleX);
+            column = column + (s32) ((tracking + 7) * ScaleX);
         } else {
-            gSPDisplayList(gDisplayListHead++, (Gfx*)D_020077D8);
+            //TextureList.push_back({nullptr, 0, 0, 0, 0, 0, true});
+            //gSPDisplayList(gDisplayListHead++, (Gfx*)D_020077D8);
             return;
         }
         if (glyphIndex >= 0x30) {
@@ -155,13 +167,13 @@ void AText::Print3D(Camera* camera, char* text, s32 tracking, f32 scaleX, f32 sc
             text += 1;
         }
     }
-    gSPDisplayList(gDisplayListHead++, (Gfx*)D_020077D8);
+    //gSPDisplayList(gDisplayListHead++, (Gfx*)D_020077D8);
 
     // @port Resume Interpolation, if interpolated later remove this tag
     FrameInterpolation_ShouldInterpolateFrame(true);
 }
 
-Gfx* AText::PrintLetter3D(Camera* camera, Gfx* arg0, MenuTexture* glyphTexture, f32 column, f32 row, s32 mode, f32 scaleX, f32 scaleY) {
+void AText::PrintLetter3D(MenuTexture* glyphTexture, f32 column, f32 row, s32 mode) {
     s32 var_v0;
     u8* temp_v0_2;
     f32 thing0;
@@ -195,42 +207,73 @@ Gfx* AText::PrintLetter3D(Camera* camera, Gfx* arg0, MenuTexture* glyphTexture, 
             if (texture->textureData != 0) {
                 switch (mode) {
                     case 1:
-                        gSPDisplayList(arg0++, (Gfx*)D_020077F8);
+                        //gSPDisplayList(arg0++, (Gfx*)D_020077F8);
                         break;
                     case 2:
-                        gSPDisplayList(arg0++, (Gfx*)D_02007818);
+                        //gSPDisplayList(arg0++, (Gfx*)D_02007818);
                         break;
                 }
+
                 TextureList.push_back({
                     (const char*)texture->textureData,
                     texture->dX + column,
                     texture->dY + row,
                     texture->width,
                     texture->height,
-                    scaleX,
-                    scaleY,
-                    mode
+                    mode,
+                    false
                 });
-                arg0 = AText::SetupMatrix(camera, arg0, (u8*)texture->textureData, texture->dX + column, texture->dY + row,
-                                    texture->width, texture->height, scaleX, scaleY);
+                WidthOffset += texture->dX + column;
+                //arg0 = AText::SetupMatrix(camera, arg0, (u8*)texture->textureData, texture->dX + column, texture->dY + row,
+                //                    texture->width, texture->height, scaleX, scaleY);
             }
             texture++;
         //}
     }
-    return arg0;
 }
 
-Gfx* AText::SetupMatrix(Camera* camera, Gfx* displayListHead, u8* texture, f32 column, f32 row, u32 width, u32 height, f32 scaleX, f32 scaleY) { // Based on func_80095BD0
+// Vtx myVtx[] = {
+//     {{{ 0, 65520, 0}, 0, {  0, 0}, {0xff, 0xff, 0xff, 0xff}}},
+//     {{{16, 65520, 0}, 0, {960, 0}, {0xff, 0xff, 0xff, 0xff}}},
+//     {{{16, 0, 0}, 0, {960, 960}, {0x00, 0x00, 0xff, 0xff}}},
+//     {{{ 0, 0, 0}, 0, {  0, 960}, {0x00, 0x00, 0xff, 0xff}}},
+//     {{{16, 0, 0}, 0, {960, 960}, {0x00, 0xff, 0x00, 0xff}}},
+//     {{{ 0, 0, 0}, 0, {  0, 960}, {0x00, 0xff, 0x00, 0xff}}},
+//     {{{16, 0, 0}, 0, {960, 960}, {0xff, 0x00, 0x00, 0xff}}},
+//     {{{ 0, 0, 0}, 0, {  0, 960}, {0xff, 0x00, 0x00, 0xff}}},
+//     {{{16, 0, 0}, 0, {960, 960}, {0xff, 0xff, 0x00, 0xff}}},
+//     {{{ 0, 0, 0}, 0, {  0, 960}, {0xff, 0xff, 0x00, 0xff}}},
+//     {{{16, 0, 0}, 0, {960, 960}, {0xff, 0x00, 0x00, 0xff}}},
+//     {{{ 0, 0, 0}, 0, {  0, 960}, {0xff, 0x00, 0x00, 0xff}}},
+//     {{{16, 0, 0}, 0, {960, 960}, {0xff, 0x00, 0x00, 0xff}}},
+//     {{{ 0, 0, 0}, 0, {  0, 960}, {0xff, 0x00, 0x00, 0xff}}},
+//     {{{16, 0, 0}, 0, {960, 960}, {0xff, 0x00, 0x00, 0xff}}},
+//     {{{ 0, 0, 0}, 0, {  0, 960}, {0xff, 0x00, 0x00, 0xff}}},
+// };
+
+Vtx letter_vtx[] = {
+    {{{ 0, 65520, 0}, 0, {  0, 0}, {0xff, 0xff, 0xff, 0xff}}},
+    {{{26, 65520, 0}, 0, {960, 0}, {0xff, 0xff, 0xff, 0xff}}},
+    {{{26, 0, 0}, 0, {960, 960}, {0, 0, 0xff, 0xff}}},
+    {{{0, 0, 0}, 0, {0, 960}, {0, 0, 0xff, 0xff}}},
+};
+
+Vtx letter_vtx2[] = {
+    {{{ 26, 65520, 0}, 0, {  0, 0}, {0xff, 0xff, 0xff, 0xff}}},
+    {{{52, 65520, 0}, 0, {960, 0}, {0xff, 0xff, 0xff, 0xff}}},
+    {{{52, 0, 0}, 0, {960, 960}, {0, 0, 0xff, 0xff}}},
+    {{{26, 0, 0}, 0, {0, 960}, {0, 0, 0xff, 0xff}}},
+};
+
+void AText::SetupMatrix(Camera* camera) { // Based on func_80095BD0
     Mat4 mf;
     Vtx* vtxPtr;
     Vec3su rot = {0, 0, 0};
 
-    f32 sp48 = sins(-camera->rot[1] - 0x8000);
+    f32 sp48 = sins((camera->rot[1]) - 0x8000);
     f32 temp_f0 = coss(camera->rot[1] - 0x8000);
 
     mf[0][0] = temp_f0;
-    mf[0][0] = scaleX;
-    
     mf[0][2] = -sp48;
     mf[2][2] = temp_f0;
     mf[1][0] = 0.0f;
@@ -244,55 +287,76 @@ Gfx* AText::SetupMatrix(Camera* camera, Gfx* displayListHead, u8* texture, f32 c
     //mf[1][1] = 1.0f;
     mf[3][3] = 1.0f; // unk3c
 
-    mf[3][0] = Pos[0] + column;
+    mf[3][0] = Pos[0]; // + tex.column;
     mf[3][1] = Pos[1]; // + row
     mf[3][2] = Pos[2];
 
-    mf[1][1] = scaleY;
-    FrameInterpolation_RecordOpenChild("actor_text", TAG_LETTER((uintptr_t) &texture << 8) + (width + height));
-    //ApplyMatrixTransformations(mf, FVector(Pos[0] + column, Pos[1] + row, Pos[2]), *(IRotator*)&Rot, FVector(1.0f, 1.0f, 1.0f));
-    //mf[0][2] = 0.0f;
-    //mf[2][0] = 0.0f;
-   //    mf[2][2] = 1.0f;
-    //mf[3][2] = 100.0f;
-    displayListHead = AddTextMatrix(displayListHead, mf);
+    // Apply scaling
+    mf[0][0] *= ScaleX;
+    mf[1][1] = -ScaleY;
 
-    gDPLoadTextureTile_4b(displayListHead++, (Gfx*)LOAD_ASSET_RAW(texture), G_IM_FMT_I, width, 0, 0, 0, width, height + 2, 0,
-                          G_TX_NOMIRROR | G_TX_WRAP, G_TX_NOMIRROR | G_TX_WRAP, G_TX_NOMASK, G_TX_NOMASK, G_TX_NOLOD,
-                          G_TX_NOLOD);
-    switch (width) {
-        default:
-            vtxPtr = (Vtx*)D_02007CD8;
-            break;
-        case 16:
-            vtxPtr = (Vtx*)D_02007CD8;
-            break;
-        case 26:
-            vtxPtr = (Vtx*)D_02007BB8;
-            break;
-        case 30:
-            vtxPtr = (Vtx*)D_02007DF8;
-            break;
+//    FrameInterpolation_RecordOpenChild("actor_text", TAG_LETTER((uintptr_t) this << 8));
+    AddObjectMatrix(mf, G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+    gSPDisplayList(gDisplayListHead++, (Gfx*)D_020077A8);
+        switch (1) {
+            case 1:
+                gSPDisplayList(gDisplayListHead++, (Gfx*)D_020077F8);
+                break;
+            case 2:
+                gSPDisplayList(gDisplayListHead++, (Gfx*)D_02007818);
+                break;
+        }
+
+    for (CharacterList& tex : TextureList) {
+        printf("tex texture %p width %d height %d mode %d col %f\n", tex.Texture, tex.width, tex.height, tex.mode, tex.column);
+        gDPLoadTextureTile_4b(gDisplayListHead++, (Gfx*)tex.Texture, G_IM_FMT_I, tex.width, 0, 0, 0, tex.width, tex.height + 2, 0,
+                            G_TX_NOMIRROR | G_TX_CLAMP, G_TX_NOMIRROR | G_TX_CLAMP, G_TX_NOMASK, G_TX_NOMASK, G_TX_NOLOD,
+                            G_TX_NOLOD);
+
+        switch (tex.width) {
+            default:
+                vtxPtr = (Vtx*)D_02007CD8;
+                break;
+            case 16:
+                vtxPtr = (Vtx*)D_02007CD8;
+                break;
+            case 26:
+                vtxPtr = (Vtx*)D_02007BB8;
+                break;
+            case 30:
+                vtxPtr = (Vtx*)D_02007DF8;
+                break;
+        }
+
+        s32 index;
+        gSPClearGeometryMode(gDisplayListHead++, G_ZBUFFER);
+        if ((s32) TextColour < TEXT_BLUE_GREEN_RED_CYCLE_1) {
+            index = TextColour;
+        } else {
+            index = ((TextColour * 2) + ((s32) gGlobalTimer % 2)) - 4;
+        }
+
+        Vtx* vtxSrc = (Vtx*)LOAD_ASSET_RAW(vtxPtr); // important copy
+        memcpy(tex.vtx, vtxSrc, sizeof(Vtx) * 2);
+
+        memcpy(&tex.vtx[2], &vtxSrc[(index + 1) * 2], sizeof(Vtx) * 2);
+
+        tex.vtx[0].v.ob[0] += (s16)(tex.column * 2.1f);
+        tex.vtx[1].v.ob[0] += (s16)(tex.column * 2.1f);
+        tex.vtx[2].v.ob[0] += (s16)(tex.column * 2.1f);
+        tex.vtx[3].v.ob[0] += (s16)(tex.column * 2.1f);
+
+        gSPVertex(gDisplayListHead++, (uintptr_t)tex.vtx, 4, 0);
+        gSPDisplayList(gDisplayListHead++, (Gfx*)common_rectangle_display);
+
+        gSPSetGeometryMode(gDisplayListHead++, G_ZBUFFER);
     }
-    displayListHead = Colour(displayListHead, vtxPtr);
-    FrameInterpolation_RecordCloseChild();
-    return displayListHead;
+
+    gSPDisplayList(gDisplayListHead++, (Gfx*)D_020077D8);
+  //  FrameInterpolation_RecordCloseChild();
 }
 
-Gfx* AText::Colour(Gfx* displayListHead, Vtx* vtxPtr) { // Based on func_800959F8
-    s32 index;
-    gSPClearGeometryMode(displayListHead++, G_ZBUFFER);
-    if ((s32) TextColour < TEXT_BLUE_GREEN_RED_CYCLE_1) {
-        index = TextColour;
-    } else {
-        index = ((TextColour * 2) + ((s32) gGlobalTimer % 2)) - 4;
-    }
+// Gfx* AText::SetupVtx(Gfx* displayListHead, Vtx* vtxPtr) { // Based on func_800959F8
 
-    Vtx* vtx = (Vtx*)LOAD_ASSET_RAW(vtxPtr);
-    gSPVertex(displayListHead++, (uintptr_t)vtx, 2, 0);
-    gSPVertex(displayListHead++, (uintptr_t)&vtx[(index + 1) * 2], 2, 2);
-    gSPDisplayList(displayListHead++, (Gfx*)common_rectangle_display);
-
-    gSPSetGeometryMode(displayListHead++, G_ZBUFFER);
-    return displayListHead;
-}
+//     return displayListHead;
+// }
--- src/engine/actors/Text.h
+++ src/engine/actors/Text.h
@@ -16,8 +16,30 @@ public:
     const char* Text;
     TextMode Mode;
     uint32_t PlayerIndex;
-    uint32_t HeightOffset = 50; // Place text above player
+    uint32_t HeightOffset = 10; // Place text above player
+    float WidthOffset = 0.0f;
     COLOR_ID TextColour;
+    f32 ScaleX = 0.55f;
+    f32 ScaleY = 0.55f;
+    bool Build = true;
+    Vtx myVerts[16] = {
+    {{{ 0, (s16)65520, 0}, 0, {  0, 0}, {0xff, 0xff, 0xff, 0xff}}},
+    {{{16, (s16)65520, 0}, 0, {960, 0}, {0xff, 0xff, 0xff, 0xff}}},
+    {{{16, 0, 0}, 0, {960, 960}, {0x00, 0x00, 0xff, 0xff}}},
+    {{{ 0, 0, 0}, 0, {  0, 960}, {0x00, 0x00, 0xff, 0xff}}},
+    {{{16, 0, 0}, 0, {960, 960}, {0x00, 0xff, 0x00, 0xff}}},
+    {{{ 0, 0, 0}, 0, {  0, 960}, {0x00, 0xff, 0x00, 0xff}}},
+    {{{16, 0, 0}, 0, {960, 960}, {0xff, 0x00, 0x00, 0xff}}},
+    {{{ 0, 0, 0}, 0, {  0, 960}, {0xff, 0x00, 0x00, 0xff}}},
+    {{{16, 0, 0}, 0, {960, 960}, {0xff, 0xff, 0x00, 0xff}}},
+    {{{ 0, 0, 0}, 0, {  0, 960}, {0xff, 0xff, 0x00, 0xff}}},
+    {{{16, 0, 0}, 0, {960, 960}, {0xff, 0x00, 0x00, 0xff}}},
+    {{{ 0, 0, 0}, 0, {  0, 960}, {0xff, 0x00, 0x00, 0xff}}},
+    {{{16, 0, 0}, 0, {960, 960}, {0xff, 0x00, 0x00, 0xff}}},
+    {{{ 0, 0, 0}, 0, {  0, 960}, {0xff, 0x00, 0x00, 0xff}}},
+    {{{16, 0, 0}, 0, {960, 960}, {0xff, 0x00, 0x00, 0xff}}},
+    {{{ 0, 0, 0}, 0, {  0, 960}, {0xff, 0x00, 0x00, 0xff}}},
+};
 
     struct CharacterList {
         const char* Texture;
@@ -25,9 +47,9 @@ public:
         f32 row;
         u32 width;
         u32 height;
-        f32 scaleX;
-        f32 scaleY;
         s32 mode;
+        bool end; // end tag
+        Vtx vtx[4];
     };
 
     std::vector<CharacterList> TextureList;
@@ -56,8 +78,14 @@ public:
     virtual void SetSpawnParams(SpawnParams& params) override;
     virtual bool IsMod() override;
     void FollowPlayer();
-    void Print3D(Camera* camera, char* text, s32 tracking, f32 scaleX, f32 scaleY, s32 mode);
-    Gfx* PrintLetter3D(Camera* camera, Gfx* arg0, MenuTexture* glyphTexture, f32 column, f32 row, s32 mode, f32 scaleX, f32 scaleY);
-    Gfx* SetupMatrix(Camera* camera, Gfx* displayListHead, u8* texture, f32 column, f32 row, u32 width, u32 height, f32 scaleX, f32 scaleY); // Based on func_80095BD0
-    Gfx* Colour(Gfx* displayListHead, Vtx* arg1);
+
+    //void Print3D(Camera* camera, char* text, s32 tracking, f32 scaleX, f32 scaleY, s32 mode);
+    //Gfx* PrintLetter3D(Camera* camera, Gfx* arg0, MenuTexture* glyphTexture, f32 column, f32 row, s32 mode, f32 scaleX, f32 scaleY);
+    //Gfx* SetupMatrix(Camera* camera, Gfx* displayListHead, u8* texture, f32 column, f32 row, u32 width, u32 height, f32 scaleX, f32 scaleY); // Based on func_80095BD0
+    //Gfx* Colour(Gfx* displayListHead, Vtx* arg1);
+    void Init();
+    void Print3D(char* text, s32 tracking, s32 mode);
+    void PrintLetter3D(MenuTexture* glyphTexture, f32 column, f32 row, s32 mode);
+    void SetupMatrix(Camera* camera); // Based on func_80095BD0
+    Gfx* SetupVtx(Gfx* displayListHead, Vtx* arg1);
 };
