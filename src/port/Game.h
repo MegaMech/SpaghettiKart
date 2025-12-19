932c7169b6c17576c695b7ef313d8a4cb7ab7760
MegaMech <MegaMech@users.noreply.github.com>
2025-12-18 17:58:36 -0700

    Update Game.h

--- src/port/Game.h
+++ src/port/Game.h
@@ -236,12 +236,11 @@ __attribute__((format(printf, 1, 2)))
 // Add noreturn attribute
 #if defined(_MSC_VER)
     __declspec(noreturn) 
-#elif defined(__clang__)
-    _Noreturn
+#elif defined(__APPLE__)
 #elif defined(__cplusplus) || (__STDC_VERSION__ >= 202311)
     [[noreturn]] 
 #elif defined(__GNUC__)
-    __attribute__((noreturn)) 
+    __attribute__((noreturn))
 #endif
 void CM_ThrowRuntimeError(const char* fmt, ...);
 
