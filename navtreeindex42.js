004ba9c55ff407c8b0f52b7e51185024b00b774f
MegaMech <MegaMech@users.noreply.github.com>
2026-03-06 06:29:12 +0000

    Deploying to gh-pages from @ MegaMech/SpaghettiKart@ba0a24e28fc652aaaa272581831ced8a69614a19 🚀

--- navtreeindex42.js
+++ navtreeindex42.js
@@ -1,5 +1,6 @@
 var NAVTREEINDEX42 =
 {
+"math__util__2_8h.html#a7ae92ff6cd417f23d4dcca06468abad7":[20,0,3,47,36],
 "math__util__2_8h.html#a7c43b4c4dff1ad4729852bf68855535d":[20,0,3,47,65],
 "math__util__2_8h.html#a7c8faeecd6f346dbd257669e248aace6":[20,0,3,47,11],
 "math__util__2_8h.html#a8831e399c95acebb71a9bd97c7de09dd":[20,0,3,47,34],
@@ -132,14 +133,14 @@ var NAVTREEINDEX42 =
 "md_docs_2BUILDING.html#windows":[2,0],
 "md_docs_2custom-audio.html":[5],
 "md_docs_2custom-audio.html#example":[5,0],
-"md_docs_2custom-audio.html#future-plans-1":[5,1],
+"md_docs_2custom-audio.html#future-plans":[5,1],
 "md_docs_2custom-characters.html":[6],
-"md_docs_2custom-characters.html#additional-files-1":[6,1],
-"md_docs_2custom-characters.html#change-viewport-render-settings-1":[6,1,0,0],
-"md_docs_2custom-characters.html#examples-for-all-characters-1":[6,0],
-"md_docs_2custom-characters.html#future-plans-2":[6,1,2],
-"md_docs_2custom-characters.html#tips-1":[6,1,0],
-"md_docs_2custom-characters.html#tools-to-help-1":[6,1,1],
+"md_docs_2custom-characters.html#additional-files":[6,1],
+"md_docs_2custom-characters.html#change-viewport-render-settings":[6,1,0,0],
+"md_docs_2custom-characters.html#examples-for-all-characters":[6,0],
+"md_docs_2custom-characters.html#future-plans-1":[6,1,2],
+"md_docs_2custom-characters.html#tips":[6,1,0],
+"md_docs_2custom-characters.html#tools-to-help":[6,1,1],
 "md_docs_2faq.html":[7],
 "md_docs_2migrations.html":[8],
 "md_docs_2migrations.html#generated-files":[8,1,1],
@@ -184,8 +185,8 @@ var NAVTREEINDEX42 =
 "md_docs_2sequences-information.html#race-tracks":[11,0],
 "md_docs_2textures-pack.html":[12],
 "md_docs_2textures-pack.html#example-1":[12,0],
-"md_docs_2textures-pack.html#future-plans-3":[12,2],
-"md_docs_2textures-pack.html#tools-to-help-2":[12,1],
+"md_docs_2textures-pack.html#future-plans-2":[12,2],
+"md_docs_2textures-pack.html#tools-to-help-1":[12,1],
 "md_docs_2track__returns.html":[13],
 "md_docs_2track__returns.html#battle-courses-1":[13,1],
 "md_docs_2track__returns.html#mario-kart-world-remixes":[13,2],
@@ -248,6 +249,5 @@ var NAVTREEINDEX42 =
 "memory_8h.html#ad108574c2862d6903a3da3c59ef4b06e":[20,0,3,10,11,9],
 "memory_8h.html#afe57de3903e2ed3acb35c29651445c5b":[20,0,3,10,11,18],
 "memory_8h_source.html":[20,0,3,10,11],
-"memory__pool_8c.html":[20,0,3,2,5],
-"memory__pool_8c.html#a30a686f067ca0076a6782a9fd8b9a2bb":[20,0,3,2,5,0]
+"memory__pool_8c.html":[20,0,3,2,5]
 };
