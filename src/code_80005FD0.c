cf3bcb52f84689ee1d73e79f0c771f7cd1ac0b6c
MegaMech <MegaMech@users.noreply.github.com>
2026-08-11 21:07:08 -0600

    Fix spellings (#743)

--- src/code_80005FD0.c
+++ src/code_80005FD0.c
@@ -3465,8 +3465,8 @@ void init_course_path_point(void) {
         if (D_80163368[i] >= 2) {
             load_track_path(i);
             calculate_track_boundaries(i);
-            analyze_track_section(i);
-            analyse_angle_path(i);
+            analyze_track_sections(i);
+            analyze_path_angle(i);
             analyze_curved_path(i);
         }
     }
@@ -3802,7 +3802,7 @@ f32 calculate_track_curvature(s32 pathIndex, u16 waypointIndex) {
     return -((temp_f10 * temp_f10_2) - (temp_f8_2 * temp_f8)) / (root2 * root1);
 }
 
-void analyze_track_section(s32 pathIndex) {
+void analyze_track_sections(s32 pathIndex) {
     f64 sectionCurvature;
     UNUSED s32 pad;
     s32 k;
@@ -3877,7 +3877,7 @@ s16 calculate_angle_path(s32 pathIndex, s32 waypointIndex) {
 }
 
 // Populates gPathExpectedRotation
-void analyse_angle_path(s32 pathIndex) {
+void analyze_path_angle(s32 pathIndex) {
     s32 waypointIndex;
     u16* angle;
 
