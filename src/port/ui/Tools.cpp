bcbe6469eab49137452c9ca8c3a7bcc2d88c3fa4
MegaMech <MegaMech@users.noreply.github.com>
2026-09-07 00:16:47 -0600

    Update Tools.cpp

--- src/port/ui/Tools.cpp
+++ src/port/ui/Tools.cpp
@@ -157,6 +157,10 @@ namespace TrackEditor {
                 }
             } else {
                 func_800CA330(0x19); // Reset audio
+                if(HMAS_IsPlaying(HMAS_MUSIC)){
+                    HMAS_AddEffect(HMAS_MUSIC, HMAS_EFFECT_VOLUME, HMAS_LINEAR, 10, 0);
+                    HMAS_AddEffect(HMAS_MUSIC, HMAS_EFFECT_STOP,   HMAS_INSTANT, 1, 0);
+                }
                 CVarSetInteger("gFreecam", true);
                 CM_SetFreeCamera(true);
             }
