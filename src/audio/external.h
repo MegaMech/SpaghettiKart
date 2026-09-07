9e37104bb40fde3d8b293ba5939f7b7128fe44c4
MegaMech <MegaMech@users.noreply.github.com>
2026-09-06 22:32:57 -0600

    Change parameter type of audio_reset_session_eu

--- src/audio/external.h
+++ src/audio/external.h
@@ -201,7 +201,7 @@ typedef struct {
 void func_800C94A4(u8);
 void func_800CADD0(u8, f32);
 void func_800C13F0(void);
-void audio_reset_session_eu(OSMesg);
+void audio_reset_session_eu(s32);
 f32 func_800C1480(u8, u8);
 s8 func_800C15D0(u8, u8, u8);
 s8 func_800C16E8(f32, f32, u8);
