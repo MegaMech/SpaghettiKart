b89b465ef931642301c8a710a8b5fd24bc652511
MegaMech <MegaMech@users.noreply.github.com>
2026-09-07 00:16:25 -0600

    Update TourCamera.cpp

--- src/engine/cameras/TourCamera.cpp
+++ src/engine/cameras/TourCamera.cpp
@@ -57,6 +57,10 @@ void TourCamera::Stop() {
     printf("[TourCamera] End of Track Tour\n");
     gTourComplete = true;
     func_800CA330(0x19); // Reset audio
+    if(HMAS_IsPlaying(HMAS_MUSIC)){
+        HMAS_AddEffect(HMAS_MUSIC, HMAS_EFFECT_VOLUME, HMAS_LINEAR, 10, 0);
+        HMAS_AddEffect(HMAS_MUSIC, HMAS_EFFECT_STOP,   HMAS_INSTANT, 1, 0);
+    }
 
     gScreenContexts[0].pendingCamera = &cameras[0];
     bActive = false;
